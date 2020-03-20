a70cc8cb1b30a018560da2e3518a55a4e7ac8634
Lukas <budgie.llfnnx@gmail.com>
2020-03-21 00:00:51 +0100

    Gui Wip

--- src/GUI.py
+++ src/GUI.py
@@ -12,6 +12,7 @@ from kivy.uix.image import Image
 from kivy.uix.label import Label
 from kivy.uix.screenmanager import ScreenManager
 from kivy.uix.widget import Widget
+from datetime import datetime
 
 kivy.require('1.11.0')
 
@@ -20,21 +21,37 @@ Window.fullscreen = False
 
 
 class MyEntry(BoxLayout):
-    pass
+    orientation = 'horizontal'
+    # padding = 20
+    spacing = 10
+
+    def build(self):
+        print('Test')
+        lbl = Label()
+        lbl.id = 'lbl'
+        lbl.text = datetime.now()
+        self.add_widget(lbl)
+        img = Image()
+        img.id = 'img'
+        img.source = 'sbahn_Transparent.png'
+        img.size = img.texture_size
+        self.add_widget(img)
+        return self
+
+
+
 
 
 class MyScreen(ScreenManager):
 
-    def get_by_id(self, p_id)-> Widget:
+    def get_by_id(self, p_id) -> Widget:
         return self.ids.get(p_id)
 
     def add_entry(self):
-        print('Test')
         myEntry = MyEntry()
         myBox = self.get_by_id('bl')
-        print(myBox)
-        print(myBox.name)
-        myBox.add_widget(myEntry)
+        myBox.add_widget(myEntry, 2)
+        print('Add')
         pass
 
 
