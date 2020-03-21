caf6f861062b20a9c27c827d902900f362497e84
Lukas <budgie.llfnnx@gmail.com>
2020-03-21 20:40:37 +0100

    Gui Wip

--- src/GUI.py
+++ src/GUI.py
@@ -20,38 +20,29 @@ kivy.require('1.11.0')
 Window.fullscreen = False
 
 
-class MyEntry(BoxLayout):
-    orientation = 'horizontal'
-    # padding = 20
-    spacing = 10
-
-    def build(self):
-        print('Test')
-        lbl = Label()
-        lbl.id = 'lbl'
-        lbl.text = datetime.now()
-        self.add_widget(lbl)
-        img = Image()
-        img.id = 'img'
-        img.source = 'sbahn_Transparent.png'
-        img.size = img.texture_size
-        self.add_widget(img)
-        return self
-
-
-
-
-
 class MyScreen(ScreenManager):
 
     def get_by_id(self, p_id) -> Widget:
         return self.ids.get(p_id)
 
     def add_entry(self):
-        myEntry = MyEntry()
         myBox = self.get_by_id('bl')
+        myEntry = BoxLayout(orientation='horizontal', padding=20, spacing=10)
+        # add Image
+        img = Image()
+        img.id = 'img'
+        img.source = 'sbahn_Transparent.png'
+        img.size = img.texture_size
+        myEntry.add_widget(img)
+        # add Label
+        lbl = Label()
+        lbl.id = 'lbl'
+        lbl.markup = True
+        lbl.text = '[size=60][color=55bb33]SBahn {}[/color][/size]'.format(datetime.now())
+        myEntry.add_widget(lbl)
+
+        # myEntry = MyEntry()
         myBox.add_widget(myEntry, 2)
-        print('Add')
         pass
 
 
